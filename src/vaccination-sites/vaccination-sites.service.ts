1ae190c6b37104c74e8a5be1fde167bffd0390f1
thanghq24 <thang.hq@zinza.com.vn>
2023-06-26 16:48:09 +0700

    fix: add relations to findOne vax site api

--- src/vaccination-sites/vaccination-sites.service.ts
+++ src/vaccination-sites/vaccination-sites.service.ts
@@ -4,10 +4,7 @@ import { UpdateVaccinationSiteDto } from './dto/update-vaccination-site.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { VaccinationSite } from 'src/entities/vaccination-site.entity';
 import { Like, Repository } from 'typeorm';
-<<<<<<< HEAD
 import { FindVaccinationDto } from './dto/find-vaccination-site.dto';
-=======
->>>>>>> fix: fix vax sites findAll
 
 @Injectable()
 export class VaccinationSitesService {
@@ -29,23 +26,10 @@ export class VaccinationSitesService {
     return this.findOne(createdVaccinationSite.id);
   }
 
-<<<<<<< HEAD
   findAll(findQuery: FindVaccinationDto) {
     const { page, pageSize, ward, district, province, name, address } =
       findQuery;
 
-=======
-  findAll(
-    page: number,
-    pageSize: number,
-    ward: number,
-    district: number,
-    province: number,
-    name: string,
-    address: string,
-  ) {
-    console.log(page, pageSize, ward, district, province, name, address);
->>>>>>> fix: fix vax sites findAll
     return this.vaccinationSiteRepository.findAndCount({
       take: pageSize,
       skip: page * pageSize,
