7c84d5c36346d553ece28a1d3a55cf3cc2aceaad
thanghq24 <thang.hq@zinza.com.vn>
2023-06-26 15:07:49 +0700

    fix: fix auth guard with role checker

---
+++ src/auth/guard/base-auth.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorator/public-route.decorator';
+
+@Injectable()
+export class BaseAuthGuard implements CanActivate {
+  constructor(protected readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+    return false;
+  }
+}
